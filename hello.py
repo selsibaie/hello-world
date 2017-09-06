66da5930f067a35265a2bd1b6f2705feb73a90cb
selsibaie <s.elsibaie@gmail.com>
2017-09-06 09:14:08 -0400

    put hello logic into function, added module support boilerplate

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 '''
 This application will print 'hello world' for the user
 '''
 
-print("hello world")
+def hello():
+    '''Prints hello world'''
+    print("hello world")
+
+if __name__ == '__main__':
+    hello()
